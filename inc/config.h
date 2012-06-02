cc07a25fcdb76d10a699420c6717bd8c55fd4bbb
Dev.Life <metalkin@tistory.com>
2012-06-03 01:44:50 +0900

    first commit

---
+++ inc/config.h
@@ -0,0 +1,16 @@
+/*
+ * config.h
+ *
+ *  Created on: 2012. 5. 9.
+ *      Author: metalkin
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define FIXED_POINT
+#define USE_KISS_FFT
+
+#define EXPORT
+
+#endif /* CONFIG_H_ */
